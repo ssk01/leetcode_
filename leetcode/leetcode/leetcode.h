6b0d489800de5f21a0f7c894d15414d2ee97a0b9
石守珂 <1640761137@qq.com>
2017-04-05 15:26:20 +0800

    fuck day

--- leetcode/leetcode/leetcode.h
+++ leetcode/leetcode/leetcode.h
@@ -13,6 +13,74 @@
 #include <unordered_set>
 using namespace std;
 
+bool isMatch(string s, string p) {
+	/**
+	* f[i][j]: if s[0..i-1] matches p[0..j-1]
+	* if p[j - 1] != '*'
+	*      f[i][j] = f[i - 1][j - 1] && s[i - 1] == p[j - 1]
+	* if p[j - 1] == '*', denote p[j - 2] with x
+	*      f[i][j] is true iff any of the following is true
+	*      1) "x*" repeats 0 time and matches empty: f[i][j - 2]
+	*      2) "x*" repeats >= 1 times and matches "x*x": s[i - 1] == x && f[i - 1][j]
+	* '.' matches any single character
+	*/
+	int m = s.size();
+	int n = p.size();
+	vector<vector<bool>> f(m + 1, vector<bool>(n + 1, false));
+	f[0][0] = true;
+	for (int i = 1; i <= m; i++)
+	{
+		f[i][0] = false;
+	}
+	for (int j = 1; j <= n; j++)
+	{
+		f[0][j] = j>1 &&(p[j - 1] == '*' && f[0][j - 2]);
+	}
+	for (int i = 1; i <= m; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			if (p[j - 1] != '*')
+			{
+				f[i][j] = f[i - 1][j - 1] && (s[i - 1] == p[j - 1] || p[j - 1] == '.');
+			}
+			else
+			{
+				f[i][j] = f[i][j - 2] || (s[i - 1] == p[j - 2] || p[j - 2] == '.') && f[i - 1][j];
+			}
+		}
+	}
+	return f[m][n];
+}
+
+
+//bool isMatch(string s, string p)
+//{
+//	// x* matches empty string or at least one character: x* -> xx*
+//	// *s is to ensure s is non-empty
+//	if (p.empty()) return s.empty();
+//	if (p.size() >=2&&p[1] == '*')
+//	{
+//		return (isMatch(s, p.substr(2)) || (!s.empty() && (s[0] == p[0] || p[0] == '.') && isMatch(s.substr(1), p)));
+//	}
+//	else
+//	{
+//		return !s.empty() && (s[0] == p[0] || p[0] == '.') && isMatch(s.substr(1), p.substr(1));
+//	}
+//}
+
+bool isPalindrome(int x) {
+	if (x < 0||(x!=0 && x%10==0))
+		return false;
+	int sum = 0;
+	do
+	{
+		sum = sum * 10 + x % 10;
+		x = x / 10;
+	} while (x > sum);
+
+	return (x == sum) || (x == sum / 10);
+}
 
 struct TreeNode {
 	int val;
