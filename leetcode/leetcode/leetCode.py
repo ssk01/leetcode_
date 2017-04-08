cb3d7b663a02b504ba48a10ff000947d5ac8e35a
石守珂 <1640761137@qq.com>
2017-04-08 10:54:27 +0800

    TO BE NO. 1

--- leetcode/leetcode/leetCode.py
+++ leetcode/leetcode/leetCode.py
@@ -1,4 +1,35 @@
 class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        k=0
+        for i in reversed(range(0, len(nums))):
+            if i >  0 and nums[i] > nums[i-1] :
+                for j in reversed(range(i, len(nums))):
+                    if nums[i-1] < nums[j]:
+                        nums[i-1], nums[j] = nums[j], nums[i-1]
+                        break
+                k=i
+                break
+        nums[k:] = nums[k:][::-1]
+                    
+                        
+
+
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        # return nums and [p[:i] + [nums[0]] +p[i:]
+        #     for p in self.permute(nums[1:])
+        #     for i in range(len(nums))] or [[]]
+        return [[n] + p
+                for i ,n in enumerate(nums)
+                for p in self.permute(nums[:i]+nums[i+1:])] or [[]]
+
     def myPow(self, x, n):
         """
         :type x: float
@@ -19,6 +50,8 @@ class Solution(object):
             res = self.myPow(x, n/2)
             return res * res
 
+
 a = Solution()
-print a.myPow(8.88023, 3)
+print a.permute([1,2,3])
+# print a.myPow(8.88023, 3)
 print " das "
