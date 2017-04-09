0b966edcba62839948c98c493fda14e1cd44a284
石守珂 <1640761137@qq.com>
2017-04-09 09:15:03 +0800

    tobi No. 1

--- leetcode/leetcode/leetCode.py
+++ leetcode/leetcode/leetCode.py
@@ -1,4 +1,28 @@
 class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if digits == "":
+            return []
+        kvmaps = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }   
+        # return reduce(lambda acc ,digit: [x + y 
+        # for x in acc for y in kvmaps[digits]], digits, [""])
+        return [x + y for x in kvmaps[digits[0]] 
+                for y in self.letterCombinations(digits[1:])] 
+
+
+
     def nextPermutation(self, nums):
         """
         :type nums: List[int]
