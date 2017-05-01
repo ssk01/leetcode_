91fe7e4e5ccf219190d3861b435719d26901aaf8
石守珂 <1640761137@qq.com>
2017-05-01 14:16:02 +0800

    lalaland

--- leetcode/leetcode/leetCode.py
+++ leetcode/leetcode/leetCode.py
@@ -18,7 +18,38 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
+import random
 class Solution(object):
+    
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.nums = nums
+
+    def reset(self):
+        """
+        Resets the array to its original configuration and return it.
+        :rtype: List[int]
+        """
+        return self.nums        
+
+    def shuffle(self):
+        """
+        Returns a random shuffling of the array.
+        :rtype: List[int]
+        """
+        nums = self.nums[:]
+        for i in range(len(nums)):
+            j = random.randrange(i, len(nums))
+            nums[i], num[j] = nums[j],nums[i]
+        return nums
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
     def reverseList(self, head):
         """
         :type head: ListNode
