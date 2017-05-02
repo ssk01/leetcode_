a815bd1d95c856d01ce9f9d9865744bbdb2f6a59
石守珂 <1640761137@qq.com>
2017-05-02 21:54:36 +0800

    fck

--- leetcode/leetcode/leetCode.py
+++ leetcode/leetcode/leetCode.py
@@ -22,7 +22,68 @@ import collections
 #         self.left = None
 #         self.right = None
 import random
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution(object):
+    def helper(self, root, sum, store, pre):
+        if not root:    
+            return 0
+        pre = pre + root.val
+
+        res = int(pre == sum) +(store[pre-sum] if (pre - sum) in store else 0)
+        if pre in store:
+            store[pre]+=1
+        else:
+            store[pre] = 1
+
+        res += self.helper(root.left, sum, store, pre) + self.helper(root.right, sum, store, pre)
+        store[pre]-=1
+        return res 
+
+
+    def pathSum(self, root, sum):
+        store = {}
+        return self.helper(root, sum, store, 0)
+    # def find_path(self, root, target):
+    #     if root:
+    #         return int(root.val == target) + self.find_path(root.left, target-root.val) 
+    #             + self.find_path(root.right, target-root.val)
+    #     return 0
+    # def pathSum(self, root, sum):
+    #     """
+    #     :type root: TreeNode
+    #     :type sum: int
+    #     :rtype: int
+    #     """
+    #     if root:
+    #         return self.find_path(root, sum) + self.pathSum(root.left, sum) + self.pathSum(root.right, sum)
+    #     return 0
+
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <=0:
+            return 0
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        lhs = 1
+        rhs = 2
+        allnum = 0
+        for i in range(3, n+1):
+            allnum = lhs + rhs
+            lhs = rhs
+            rhs = allnum
+        return allnum
+                    
     def singleNumber(self, nums):
         """
         :type nums: List[int]
